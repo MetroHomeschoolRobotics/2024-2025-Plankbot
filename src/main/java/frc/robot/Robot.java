6c8b9a8ff774c48c17c0eae61878898605bbcdc8
George <code@metrohomeschoolrobotics.org>
2024-11-09 23:44:29 -0600

    Addition of WPI Logging in robotInit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,10 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
+// WPI Logging Imports
+import edu.wpi.first.wpilibj.DataLogManager;
+
+
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -31,6 +35,12 @@ public class Robot extends TimedRobot {
     //m_robotContainer = new RobotContainer();
     m_robot = new SysIdRoutineBot();
     m_robot.configureBindings();
+
+    // Addition of WPI logging -George Horsey 2024-11-09 
+    // Starts recording to data log
+    DataLogManager.start();
+    
+
   }
 
   /**
